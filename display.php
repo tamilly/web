ea7b8a7e4fa83f34fe60b1da660bc08d1beb345d
Tamilly Nascimento <myllacontato@gmail.com>
2021-06-25 20:49:16 -0300

    Update display and register page

--- display.php
+++ display.php
@@ -1,4 +1,32 @@
 <?php
     session_start();
+    echo "Data stored on sessions: ";
     print_r($_SESSION["name"]);
+    echo "<br/>";
+    print_r($_SESSION["phone"]);
+
+    echo "<br/> Now let's see data stored on Database: ";
+
+    //create connection
+    $con = mysqli_connect("localhost", "root", "", "pweb");
+    //check connection
+    if(!$con){
+        die("Connection failed: ". mysqli_connect_error());
+    }
+    //SQL SELECT query 
+    //try to get data from database
+        $sql = "SELECT * FROM users";
+        $result = mysqli_query($con, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+        //output data of each row
+        while($row = mysqli_fetch_assoc($result)) {
+            echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["phone"]. "<br>";
+        }
+        } else {
+            echo "0 results";
+        }
+
+        //close connection
+        mysqli_close($con);
 ?>
--- register.php
+++ register.php
@@ -26,20 +26,7 @@
         }else{
             echo "ERROR! Sorry :( ". $sql . "<br/>" . mysqli_error($con);
         }
-        //try to get data from database
-        $sql = "SELECT * FROM users";
-        $result = mysqli_query($con, $sql);
-
-        if (mysqli_num_rows($result) > 0) {
-        //output data of each row
-        while($row = mysqli_fetch_assoc($result)) {
-            echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["phone"]. "<br>";
-        }
-        } else {
-            echo "0 results";
-        }
-
-
+        
         //close connection
         mysqli_close($con);
     }
