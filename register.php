20979a130d5ca2ebcf11b0bd92b3ff9bd2583b59
Tamilly Nascimento <myllacontato@gmail.com>
2021-06-25 19:53:59 -0300

    Added a new and better SQL query to insert data... Now it works "perfectly". At 42th line it gets user data from database.

--- register.php
+++ register.php
@@ -28,14 +28,27 @@
         /*else{
             echo "that's ok, baby";
         }*/
-        $sql = "INSERT INTO users VALUES('1','$name', '$phone')";
+        $sql = "INSERT INTO users (name, phone) VALUES ('$name', '$phone')";
 
         if(mysqli_query($con, $sql)){
             echo "it's ok, baby <br/>";
-            echo n12br("\n$name\n$phone");
         }else{
-            echo "Sorry $sql";
+            echo "ERROR! Sorry :( ". $sql . "<br/>" . mysqli_error($con);
         }
+        //try to get data from database
+        $sql = "SELECT * FROM users";
+        $result = mysqli_query($con, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+        //output data of each row
+        while($row = mysqli_fetch_assoc($result)) {
+            echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["phone"]. "<br>";
+        }
+        } else {
+            echo "0 results";
+        }
+
+
         //close connection
         mysqli_close($con);
     }
